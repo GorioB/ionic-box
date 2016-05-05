6d1be5b5c4c794df646cb0f11de7cd43435b1ec9
gorio <gmcbuencamino@gmail.com>
2016-05-06 01:07:03 +0800

    switch to 64 bit, latest android

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
-ANDROID_SDK_FILENAME=android-sdk_r24.2-linux.tgz
+ANDROID_SDK_FILENAME=android-sdk_r24.4.1-linux.tgz
 ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
 
-#sudo apt-get install python-software-properties
-#sudo add-apt-repository ppa:webupd8team/java
 apt-get update
 apt-get install -y npm git openjdk-7-jdk ant expect
 npm install -g n
-n stable
+n 5.11.0
+curl -0 -L http://npmjs.org/install.sh | sh
+rm /usr/bin/npm
+ln -s /usr/local/lib/node_modules/npm/cli.js /usr/bin/npm
 
 curl -O $ANDROID_SDK
 tar -xzvf $ANDROID_SDK_FILENAME
-sudo chown -R vagrant android-sdk-linux/
+chown -R vagrant android-sdk-linux/
 
 echo "ANDROID_HOME=~/android-sdk-linux" >> /home/vagrant/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386" >> /home/vagrant/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64" >> /home/vagrant/.bashrc
 echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /home/vagrant/.bashrc
 
 npm install -g cordova
@@ -28,5 +29,3 @@ expect {
     eof
 }
 '
-
-#sudo gem install sass
